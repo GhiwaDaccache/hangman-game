574cc4cd0c6a3d3cf4a76d5a6bd51c860903f70d
Ghiwa El Daccache <ghiwadaccache4@gmail.com>
2024-03-03 11:48:10 +0200

    fillAnswerSection declared

--- scripts/game.js
+++ scripts/game.js
@@ -1,3 +1,15 @@
 const words = ['numbers', 'cars', 'bicycle', 'chocolate', 'mountain']
 const randomWord = words[Math.floor(Math.random() * words.length)]; 
 
+const wordLength = randomWord.length
+const answerSection = document.getElementById('answer-section')
+
+console.log(randomWord)
+function fillAnswerSection(wordLength){
+    answerSection.innerHTML = "";
+    for (let i = 0; i < wordLength; i++) {
+        answerSection.innerHTML = "<span>_</span>";
+      }
+}
+
+fillAnswerSection(wordLength)
